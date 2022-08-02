e6587ce1f33b80ec508997b769b9f8893deae39d
uceiifali <musiicxxx@gmail.com>
2022-08-02 22:32:22 +0200

    first

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { BsInstagram } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
+import { HiDocumentAdd } from "react-icons/hi";
+const SocialMedia = () => {
+  return (
+    <div className="app__social">
+      <div>
+        <a href="" download="../assets/file/Youssef ali.pdf">
+          <HiDocumentAdd />
+        </a>
+      </div>
+      <div>
+        <a href="https://github.com/uceiifali">
+          <AiFillGithub />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.facebook.com/uceiif/">
+          <FaFacebookF />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.instagram.com/uceiifalii/">
+          <BsInstagram />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default SocialMedia;
---
+++ src/container/Test/Test.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Test.scss";
+const Test = () => {
+  return <div>Test</div>;
+};
+
+export default Test;
